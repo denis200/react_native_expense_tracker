74aa5564b8a15b5e34963cf09be74aa0c5a116b3
denis200 <soundze@mail.ru>
2022-03-06 11:30:19 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack"
+import {StyleSheet} from "react-native-web"
+import HomePage from "./screens/HomePage.js"
+import {NavigationContainer} from "@react-navigation/native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator()
+
+function MyStack() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomePage} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+})
+
+export default MyStack
---
+++ screens/HomePage.js
@@ -0,0 +1,10 @@
+import react from "react"
+import {StyleSheet, Text, View} from "react-native"
+
+export default function HomePage() {
+	return (
+		<View>
+			<Text style={{textAlign: "center"}}> Hello world </Text>
+		</View>
+	)
+}
